77d7a4558b533efa31f081db442aa46772c78ed9
Patrick Burtchaell <patrick@pburtchaell.com>
2020-03-15 11:34:21 -0700

    Release version 0.3.0

--- src.js
+++ src.js
@@ -1,7 +1,107 @@
 const visit = require('unist-util-visit')
 
-module.exports = ({ markdownAST }, options) => {
-    const tagClasses = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'ul', 'ol', 'img', 'blockquote']
-    console.log(markdownAST)
+const { getTypeByAlias } = require('./utils')
+const { remarkPluginOptions } = require('./aliases')
+
+module.exports = ({ markdownAST }, pluginOptions) => {
+    const rootClasses = pluginOptions.root
+    const tagClasses = pluginOptions.tag
+    const remarkClasses = pluginOptions.remark
+
+    /**
+     * Loop through the top level nodes in
+     * root to prevent duplicated/nested
+     * classes, e.g. for paragraphs.
+     */
+    markdownAST.children.forEach((node, i) => {
+        if (
+            node.type === 'html' ||
+            (node.children && node.children[0].type === 'html')
+        ) {
+            // skip plugin generated and inline HTML
+            return
+        }
+
+        /**
+         * Search for definite node root types.
+         */
+        if (rootClasses) {
+            const typeNames = Object.keys(rootClasses)
+            typeNames.forEach(name => {
+                if (node.type === name) {
+                    node = {
+                        type: 'div',
+                        data: {
+                            hProperties: {
+                                className: rootClasses[name],
+                            },
+                        },
+                        children: [node],
+                    }
+                }
+            })
+        }
+
+        // override linked object node
+        markdownAST.children[i] = node
+    })
+
+    /**
+     * Search for each node types by an alias (tag) globally.
+     */
+    if (tagClasses) {
+        const tags = Object.keys(tagClasses)
+        tags.forEach(name => {
+            const tagType = getTypeByAlias(name) /** @return {string} e.g. heading */
+
+            switch (tagType) {
+                case 'heading':
+                    const depth = Number(name[1]) // get depth from tag name `h1`[1]
+                    const isHDepth = node =>
+                        node.type === 'heading' && node.depth === depth
+                    visit(markdownAST, isHDepth, node => {
+                        if (!node.data) node.data = {}
+                        node.data.hProperties = { className: tagClasses[name] }
+                    })
+                    break
+
+                default:
+                    visit(markdownAST, tagType, node => {
+                        if (!node.data) node.data = {}
+                        node.data.hProperties = { className: tagClasses[name] }
+                    })
+                    break
+            }
+        })
+    }
+
+    /**
+     * Search globally in AST because the most time
+     * those plugin generated stuff is inside a
+     * paragraph node.
+     */
+    if (remarkClasses) {
+        const plgNames = Object.keys(remarkClasses)
+        plgNames.forEach(name => {
+            if (Object.keys(remarkPluginOptions).indexOf(name) > -1) {
+                const { type, value } = remarkPluginOptions[name]
+
+                switch (type) {
+                    case 'html':
+                        visit(markdownAST, 'html', node => {
+                            if (node.value.includes(value)) {
+                                node.value = `<div class="${remarkClasses[name]}">${node.value}</div>`
+                            }
+                        })
+                        break
+
+                    default:
+                        // noop
+                        break
+                }
+            }
+        })
+    }
+
     return markdownAST
 }
